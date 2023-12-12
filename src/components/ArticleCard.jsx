0d02b3e22a4329574bbb429d8754dbe3b234586e
holyseo <128103221+holyseo@users.noreply.github.com>
2023-12-12 10:25:21 +0000

    try to use Link

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,6 +1,9 @@
+import { Link } from "react-router-dom";
+
 const ArticleCard = ({ article }) => {
   return (
     <div className="article_cards">
+      <Link to=``></Link>
       <h4>{article.title}</h4>
       <p>Topic = {article.topic}</p>
       <p>Author = {article.author}</p>
