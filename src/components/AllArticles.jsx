fbba575923c17c8416d617f041da899eb6679c3b
holyseo <128103221+holyseo@users.noreply.github.com>
2023-12-12 09:48:37 +0000

    add isLoading

--- src/components/AllArticles.jsx
+++ src/components/AllArticles.jsx
@@ -5,20 +5,27 @@ import ArticleCard from "./ArticleCard";
 const AllArticles = () => {
   const [allArticles, setAllArticles] = useState([]);
 
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     getAllArticles().then(({ articles }) => {
       setAllArticles(articles);
+      setIsLoading(true);
     });
   }, []);
 
   return (
     <>
       <h3>A list of all articles</h3>
-      <div className="all_articles">
-        {allArticles.map((article) => {
-          return <ArticleCard key={article.article_id} article={article} />;
-        })}
-      </div>
+      {isLoading ? (
+        <div className="all_articles">
+          {allArticles.map((article) => {
+            return <ArticleCard key={article.article_id} article={article} />;
+          })}
+        </div>
+      ) : (
+        <h3>Loading all articles...</h3>
+      )}
     </>
   );
 };
